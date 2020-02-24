b06ada002a0c29eeb086453c6be2b5f5bb927182
barja8 <antony.barja8@gmail.com>
2020-02-23 22:30:47 -0500

    Reforzing R

--- Functions/script/Functions_01.R
+++ Functions/script/Functions_01.R
@@ -4,4 +4,23 @@
 help(mean)
 ?mean()
 
+# Use some basic function of R --------------------------------------------
+temp <- c(10.2, 9.8, 11.2, 12.3, 10.1, 12.3)
+pp <- c(102.2, 100.1, 103.4, 120.34, 120.3,121.1)
+
+# What is the max, min ,sd and mean of this variables? 
+max(temp)
+max(pp)
+
+min(temp)
+min(pp)
+
+sd(temp)
+sd(pp)
+
+mean(temp)
+mean(pp)
+
+
+
 
