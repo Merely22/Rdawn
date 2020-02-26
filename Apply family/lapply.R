528e0c37aa3ec125cd9ad456407e6b5ba1d74bc8
barja8 <antony.barja8@gmail.com>
2020-02-26 18:39:30 -0500

    apply family

--- Apply family/lapply.R
+++ Apply family/lapply.R
@@ -1,15 +1,15 @@
-# Introduction to apply family --------------------------------------------
+# 1.Introduction to apply family ------------------------------------------
 
 animes <- c('Death Note', 'Kimetzu no yaiba', 
             'Bleach', 'Naruto', 'Guns', 'Asura')
 
-# Using loop print
+# 1.1 Using loop print ----------------------------------------------------
 
-for( name in animes){
+ for( name in animes){
   print(paste0('My favourite animes is : ',name))
 }
 
-# Using apply family
+# 1.2 Using apply family --------------------------------------------------
 
 print_out <- function(x){
   print(paste0('My favourite animes is :', x))
@@ -17,13 +17,32 @@ print_out <- function(x){
 
 unlist(lapply(animes,print_out))
 
-# You can to use an anonymous function in lapply
+
+# 1.3 You can to use an anonymous function in lapply ----------------------
 
 unlist(lapply(animes, 
        function(x){
          print(paste0('My favourite animes is :', x))
          })
        )
+
+# 1.4 Other examples: -----------------------------------------------------
+
+memories <- c('Heydi:07', 'Milagros:10', 'Sol:13','Antonella:15',
+              'Maria Rosa:16','Geena:17', 'Melani:19')
+
+tosplit <- strsplit(memories, ":")
+
+extract <- function(x, index){ 
+  x[index]
+  }
+
+names <- lapply(tosplit,extract,index = 1)
+unlist(names)
+
+year <- lapply(tosplit, extract, index = 2)
+as.numeric(unlist(year))
+
 
 
 
