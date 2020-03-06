60d1453365d42bbfe2502300b701d1cf83bcb67e
Merely22 <Merely.tumbalobos@unmsm.edu.pe>
2020-03-06 00:06:07 -0500

    avance 07/03/2020

--- conditionals/PrimeraCondicional.R
+++ conditionals/PrimeraCondicional.R
@@ -1,4 +1,17 @@
 
 # Primera condicional -----------------------------------------------------
+#
+edad <- 20
+if(edad <= 20){
+  print(paste('pertenecen a la categoria jovenes'))
+} else {
+  print(paste('pertenecen a la categoria adulta'))
+}
 
-
+pp <- 10 
+tem <- 2
+if( pp>100&tem>30){
+  print(paste('lluvias intensas'))
+} else{
+  print(paste('lluvias temporales'))
+}
